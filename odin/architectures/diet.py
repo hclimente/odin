7e61488794c3b525468f2500fac19065be9ad316
hclimente <hclimente@gmail.com>
2018-06-08 11:48:03 +0900

    added new architectures

---
+++ odin/architectures/diet.py
@@ -0,0 +1,6 @@
+class DietNetwork():
+
+	# From Romero, A. et al. (2016). Diet Networks: Thin Parameters for Fat Genomics. 1–11.
+
+	def __init__(self):
+		pass
---
+++ odin/architectures/dnp.py
@@ -0,0 +1,6 @@
+class DeepNeuralPursuit():
+
+	# From Liu, B. et al. (2015). Deep Neural Networks for High Dimension, Low Sample Size Data. 2287–2293.
+
+	def __init__(self):
+		pass
